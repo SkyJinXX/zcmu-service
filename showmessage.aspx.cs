ebaa8b18e419886b0bfd83edc146f3a85f517324
guanyangfan <Skyjin@outlook.com>
2017-11-12 20:12:19 +0800

    删除多余文件

--- showmessage.aspx.cs
+++ showmessage.aspx.cs
@@ -15,10 +15,17 @@ public partial class showmessage : System.Web.UI.Page
     {
         objConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
         objConnection.Open();
-        String SqlStr = "Select article From announcement Where title = '" +(String)Session["name"] + "'";
+
+        String Sq = "Select documentname From announcement Where title = '" + (String)Session["name"] + "'";
+        SqlCommand cmd1 = new SqlCommand(Sq, objConnection);
+        String mm = (String)cmd1.ExecuteScalar();
+
+        String SqlStr = "Select article From announcement Where title = '" +(String)Session["name"] + "' and documentname='"+mm+"'";
         SqlCommand cmd = new SqlCommand(SqlStr, objConnection);
         Label1.Text = (String)cmd.ExecuteScalar();
         Label4.Text = (String)Session["name"];
+
+        
         objConnection.Close();
     }
 
